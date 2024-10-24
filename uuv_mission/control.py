8e2db4804f2539af398f41b1e593f0db9c00630e
Daniel Waterson <daniel.waterson@ox.ac.uk>
2024-10-24 12:34:56 +0100

    Initial implimentation of controller

---
+++ uuv_mission/control.py
@@ -0,0 +1,14 @@
+class Controller:
+    def __init__(self, kp, kd):
+        self.kp = kp
+        self.kd = kd
+        self.previous_error = 0
+    
+    def get_error(self, position, target):
+        
+        error = position - target
+        return error
+
+    def get_action_PD(self, error, t):
+        action = self.kp * error[t] + self.kd * (error[t] - error[t-1])
+        return action
